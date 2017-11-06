eda9367981b2b4d6ebba51fd8746e2ef588e0e46
Elizabeth <lizareji23@gmail.com>
2017-11-05 20:33:58 -0500

    Added jQuery, implemented flickr api and parsed response to get image urls.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ var elizabeth = User("liza_sonny", "Elizabeth Sonny", "lizareji23@gmail.com", "l
 document.getElementById("user-name").innerHTML = elizabeth.userName
 document.getElementById("name").innerHTML = elizabeth.name;
 document.getElementById("description").innerHTML = elizabeth.description;
+
+
+
+service.getImages(function (data) {
+	var images = data["items"];
+	for (var i = 0; i < images.length; i++) {
+		var image = images[i];
+		var media = image["media"]
+		console.log(media["m"]);
+	}
+	
+});
---
+++ js/service.js
@@ -0,0 +1,15 @@
+var service = {}
+
+service.getImages = function (callBack) {
+	self.callBack = callBack;
+	var apiUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=cats&format=json';
+
+	$.ajax({
+		url: apiUrl,
+		dataType: "jsonp"
+	});
+}
+
+function jsonFlickrFeed(data) {
+	self.callBack(data);
+}
