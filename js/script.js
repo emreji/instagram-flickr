2718121a5ac74181473850041ae2cba277ff5f37
Elizabeth <lizareji23@gmail.com>
2017-11-08 19:23:56 -0500

    Added modal for each image.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var modal = document.getElementById("myModal");
 var elizabeth = new User("liza_sonny", "Elizabeth Sonny", "lizareji23@gmail.com", "liz@2017", "Software Engineer");
 var images = [];
 
@@ -34,11 +35,49 @@ function displayImages(images) {
 	var imageHTML = ""
 	for (var i = 0; i < images.length; i++) {
 		var image = images[i];
-		imageHTML += "<li><img src='" + image.url + "'></li>";
+		imageHTML += '<li><img src="' + image.url + '" onclick="showModalWithIndex(' + i + ')"></li>';
 		$("#images-array").html(imageHTML);
-		
 	}
 }
 
+function showModalWithIndex(imageIndex) {
+	var modalImage = document.getElementById("modalImage");
+
+	modal.style.display = "block";
+    modalImage.src = images[imageIndex].url;
+    $("body").css("overflow", "hidden");
+}
+
+function showModalWithImageId(imageId) {
+	var img = document.getElementById(imageId);
+	var modalImage = document.getElementById("modalImage");
+
+	img.onclick = function() {
+		modal.style.display = "block";
+	    modalImage.src = this.src;
+	    $("body").css("overflow", "hidden");
+	}
+}
+
+function configureModal() {
+	var span = document.getElementsByClassName("close")[0];
+
+	span.onclick = function() {
+		modal.style.display = "none";
+		$("body").css("overflow", "auto");
+	}
+
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	        $("body").css("overflow", "auto");
+	    }
+	}
+
+}
+
+configureModal();
+showModalWithImageId("profile-image");
+showModalWithImageId("banner-image");
 displayUserInfo(elizabeth);
 getUserImages();
